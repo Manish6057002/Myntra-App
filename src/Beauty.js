f786bdb29cafa4cf47ac3d4f47219eb898c73617
Manish6057002 <158477215+Manish6057002@users.noreply.github.com>
2024-02-20 19:08:19 +0530

    Add files via upload

---
+++ src/Beauty.js
@@ -0,0 +1,15 @@
+import Product from "./Products";
+import Beautybody from "./Beautybody";
+
+function Beauty(){
+    
+    return(
+        <div className="beauty">
+            <Beautybody/>
+            <Product/>
+          
+        </div>
+
+    );
+}
+export default Beauty;
