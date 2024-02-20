56c20d5b420245045553a392badb74808a0a4d12
Manish6057002 <158477215+Manish6057002@users.noreply.github.com>
2024-02-20 19:09:21 +0530

    Add files via upload

---
+++ src/Products.js
@@ -0,0 +1,127 @@
+function Product() {
+    return (
+        <div className="product">
+            <div className="sidebar">
+                <div className="genders">
+                    <div className="gender"><input type="radio" />&nbsp;&nbsp;Men</div>
+                    <div className="gender"><input type="radio" />&nbsp;&nbsp;Women</div>
+                    <div className="gender"><input type="radio" />&nbsp;&nbsp;Boys</div>
+                    <div className="gender"><input type="radio" />&nbsp;&nbsp;Girls</div>
+                </div>
+                <div className="categories">
+                       <div className="cat">
+                        <h4>CATEGORIES</h4>
+                        <i class='bx bx-search'></i>
+                    </div>
+                    <div className="list1"><input type="checkbox" />&nbsp;&nbsp;Lipstick</div>
+                    <div className="list1"><input type="checkbox" />&nbsp;&nbsp;Nail Polish</div>
+                    <div className="list1"><input type="checkbox" />&nbsp;&nbsp;Perfume</div>
+                    <div className="list1"><input type="checkbox" />&nbsp;&nbsp;Massage Oils</div>
+                    <div className="list1"><input type="checkbox" />&nbsp;&nbsp;Face Wash and Cleanser</div>
+                    <div className="list1"><input type="checkbox" />&nbsp;&nbsp;Bindi</div>
+                    <div className="list1"><input type="checkbox" />&nbsp;&nbsp;Makeup Combo</div>
+                    <div className="list1"><input type="checkbox" />&nbsp;&nbsp;Serum and Gel</div>
+                    <h5>+175 more</h5>
+                </div>
+                <div className="brands">
+                    <div className="brand"><h3>BRAND</h3>
+                        <i class='bx bx-search'></i>
+
+                    </div>
+                    <div className="list2"><input type="checkbox" />&nbsp;&nbsp;Comet Busters</div>
+                    <div className="list2"><input type="checkbox" />&nbsp;&nbsp;MI FASHION</div>
+                    <div className="list2"><input type="checkbox" />&nbsp;&nbsp;NOY</div>
+                    <div className="list2"><input type="checkbox" />&nbsp;&nbsp;PERPAA</div>
+                    <div className="list2"><input type="checkbox" />&nbsp;&nbsp;Deve Herbes</div>
+                    <div className="list2"><input type="checkbox" />&nbsp;&nbsp;ME-ON</div>
+                    <div className="list2"><input type="checkbox" />&nbsp;&nbsp;Menjeweli</div>
+                    <div className="list2"><input type="checkbox" />&nbsp;&nbsp;FORFOR</div>
+                </div>
+            </div>
+            <div className="productlist">
+                <div className="products">
+                    <div className="image">
+                        <img src="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/24809836/2023/9/11/0213468d-37a3-4a70-aa32-0eba1e5daafd1694410668227ParachuteAdvansedCocoaRepairBodyLotionIntenseMoisture400ml251.jpg" alt="" />
+                    </div>
+                    <div className="content">
+                        <p>Livon</p>
+                        <p>Rs.340</p>
+                    </div>
+
+                </div>
+                <div className="products">
+                    <div className="image">
+                        <img src="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/15506300/2023/9/19/290c44d1-9d60-49b9-934d-102ffd2e2fb91695125968412-Lakme-Lumi-Skin-Cream-30-g-4181695125968115-1.jpg" alt="" />
+                    </div>
+                    <div className="content">
+                        <p>Livon</p>
+                        <p>Rs.340</p>
+                    </div>
+
+                </div>
+                <div className="products">
+                    <div className="image">
+                        <img src="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/18795736/2022/6/21/9d69aec3-32c1-4100-9058-8ee666080d231655789495916JOYRevivifyHyaluronicWatermelon24HDeepMoistureHydratingSerum1.jpg" alt="" />
+                    </div>
+                    <div className="content">
+                        <p>Livon</p>
+                        <p>Rs.340</p>
+                    </div>
+
+                </div>
+                <div className="products">
+                    <div className="image">
+                        <img src="https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/11915238/2021/10/11/e97689dd-24fb-4336-bba7-8b1ea7bb302f1633932426722-Philips-Men-BT310115-Series-3000-Beard-Trimmer---White-25616-1.jpg" alt="" />
+                    </div>
+                    <div className="content">
+                        <p>Livon</p>
+                        <p>Rs.340</p>
+                    </div>
+
+                </div>
+                <div className="products">
+                    <div className="image">
+                        <img src="https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/9548051/2021/11/8/ba0a240e-11c2-4442-8217-9cd00352325a1636364762901-Nova-Men-NHT-1076-Cordless-Trimmer---Black-3011636364762209-1.jpg" alt="" />
+                    </div>
+                    <div className="content">
+                        <p>Livon</p>
+                        <p>Rs.340</p>
+                    </div>
+
+                </div>
+                <div className="products">
+                    <div className="image">
+                        <img src="https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/18766448/2023/9/28/8327a47f-eba7-4aaf-a8a6-551df55ecac11695890346259-everteen-Soft-Neem--Safflower-XXL-Sanitary-Napkins-40pc-2591-1.jpg" alt="" />
+                    </div>
+                    <div className="content">
+                        <p>Livon</p>
+                        <p>Rs.340</p>
+                    </div>
+
+                </div>
+                <div className="products">
+                    <div className="image">
+                        <img src="https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/10696774/2023/2/22/74c7c414-f98f-4f26-a3ba-81c1dbe6c0a31677061137616GarnierColorNaturalsUltraHairColor-GoldenBrown731.jpg" alt="" />
+                    </div>
+                    <div className="content">
+                        <p>Livon</p>
+                        <p>Rs.340</p>
+                    </div>
+
+                </div>
+                <div className="products">
+                    <div className="image">
+                        <img src="https://assets.myntassets.com/dpr_2,q_60,w_210,c_limit,fl_progressive/assets/images/15580324/2023/12/14/ae57eba1-4115-463a-9d21-c5cdea1a98091702555999104-SWISS-BEAUTY-Long-Lasting-Makeup-Fixer-Natural-Spray---Aloe--1.jpg" alt="" />
+                    </div>
+                    <div className="content">
+                        <p>Livon</p>
+                        <p>Rs.340</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    );
+}
+export default Product;
